ea4d6c34cc9977d7b45df4fcd3e9d6041990d662
unknown <89380041414@gmail.com>
2022-04-08 11:41:20 +0300

    Сохранение данных при повороте

--- app/src/main/java/com/example/myintensivetask24/MainActivity.kt
+++ app/src/main/java/com/example/myintensivetask24/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.myintensivetask24
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myintensivetask24.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var conter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        with(binding) {
+            btn.setOnClickListener {
+                conter++
+                tv.text = conter.toString()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val saveNumber = conter
+        outState.putInt(COUNTER_SAVE, conter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val counterInt = savedInstanceState.getInt(COUNTER_SAVE, 0)
+        conter = counterInt
+        binding.tv.text = conter.toString()
+    }
+
+    companion object {
+        const val COUNTER_SAVE = "COUNTER_SAVE"
     }
 }
